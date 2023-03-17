b611e4acb96854c9abca07cef760244cb5d6619d
anaarezo <laura.arezo@gmail.com>
2023-03-16 23:28:07 -0300

    feat: styled-components config

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,46 @@
-import React, {ScrollView, View, Text} from 'react-native';
+import React, {FlatList} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-// import * as S from './styles';
+import * as S from './styles';
+
+const data = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  // {
+  //   id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+  //   title: 'Second Item',
+  // },
+  // {
+  //   id: '58694a0f-3da1-471f-bd96-145571e29d72',
+  //   title: 'Third Item',
+  // },
+];
+
+type ItemProps = {title: string};
+
+const Item = ({title}: ItemProps) => (
+  <S.Card>
+    <S.ProductPhoto></S.ProductPhoto>
+    <S.Title>Title</S.Title>
+    <S.UserAvatar></S.UserAvatar>
+    <S.UserName>Name</S.UserName>
+    <S.Rating>5.1</S.Rating>
+    <S.Distance>10mi</S.Distance>
+    <S.Viewed>12</S.Viewed>
+  </S.Card>
+);
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>Home</Text>
-        </View>
-      </ScrollView>
+      <S.Container>
+        <FlatList
+          data={data}
+          renderItem={({item}) => <Item title={item.title} />}
+          keyExtractor={item => item.id}
+        />
+      </S.Container>
     </SafeAreaView>
   );
 };
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  margin: 0;
+`;
+
+export const Card = styled.View`
+  margin: 0 15px;
+  border-radius: 13px;
+  border: 1px red solid;
+  border-radius: 20;
+`;
+
+export const ProductPhoto = styled.View`
+  background-color: red;
+`;
+
+export const Title = styled.Text`
+  font-size: 11px;
+`;
+
+export const UserAvatar = styled.View`
+  background-color: red;
+`;
+
+export const UserName = styled.Text`
+  font-size: 11px;
+`;
+
+export const Rating = styled.Text`
+  font-size: 11px;
+`;
+
+export const Distance = styled.Text`
+  font-size: 11px;
+`;
+
+export const Viewed = styled.Text`
+  font-size: 11px;
+`;
+
+export const AddedTime = styled.Text`
+  font-size: 11px;
+`;
